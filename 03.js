d80a74952dfe775af16578adc1f8e3cebf788328
caroliniicristini <carolinicristini1@gmail.com>
2023-08-21 21:24:52 -0300

    Terceiro desafio concluído

---
+++ 03.js
@@ -0,0 +1,16 @@
+let salario = prompt("Digite seu salário");
+let emprestimo = prompt("Digite o valor do empréstimo que deseja")
+
+let valorPrestacao = salario * 0.3 
+
+
+console.log(valorPrestacao)
+if(valorPrestacao > emprestimo){
+    alert("Emprestimo concedido!")
+} else{
+    alert("Emprestimo negado!")
+}
+
+
+//> maior
+//<menor 
