444d0b69dc02f31c902e7f6b20417d2381759b6f
caroliniicristini <carolinicristini1@gmail.com>
2023-08-31 12:03:18 -0300

    Organização dos exercícios em pastas

---
+++ loops/01.js
@@ -0,0 +1,50 @@
+// const states = [
+//     'Alabama',
+//     'Alaska',
+//     'Arizona',
+//     'Arkansas',
+//     'California',
+//     'Colorado',
+//     'Connecticut',
+//     'Delaware',
+//      'Florida',
+//      'Georgia'
+// ];
+
+// for (let i = 1; i <= states.length; i++){
+//     console.log(states[states.length - i])
+// }
+
+// for(let i = states.length - 1; i <= 0; i++){
+//     console.log(states[states.length - i])
+// }
+
+// for(let i = 10; i >= 0; i--){
+//     console.log(i)
+// }
+
+// for(let i = 10; i >= -5; i--){
+//     console.log(i)
+// }
+
+// for(let i = 9; i >= -30; i -= 3){ 
+//     console.log(i)
+// }
+// let n = 1;
+
+// while(n < 1){   
+//     console.log(n)
+//     n++
+
+    //adicionar +1
+// }
+
+// console.log("O loop acabou")
+
+
+function renderUpToTwnty(){
+    for(let i = 0; i <= 20; i++){
+        console.log(i)
+    }
+}
+renderUpToTwnty()
