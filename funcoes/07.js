034f47e7abe539bea0f060fc888e3727cd5129f4
caroliniicristini <carolinicristini1@gmail.com>
2023-09-07 21:02:11 -0300

    Desafios concluído!!

--- funcoes/07.js
+++ funcoes/07.js
@@ -1,3 +1,13 @@
-function notaProvas(){
+function notaProvas(matematica, portugues, informatica, conhecimentosGerais){
 
+    let pesoMatematica = 2
+    let pesoPadrao = 1;
+
+    let notas = (matematica * pesoMatematica) + (portugues * pesoPadrao) + (informatica * pesoPadrao) + (conhecimentosGerais * pesoPadrao)
+    let somaPesos = pesoMatematica + (3 * pesoPadrao)
+    let mediaPonderada = notas / somaPesos
+
+    console.log(mediaPonderada)
 }
+
+notaProvas(7, 5, 10, 8)
